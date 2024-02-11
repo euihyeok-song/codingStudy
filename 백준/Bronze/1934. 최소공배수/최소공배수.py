51633fd7000e35cba899e679e70519efa5dcadd6
Eui Hyeok Song <dmlgur0517@daum.net>
2024-02-11 15:23:08 +0900

    [Bronze I] Title: 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,14 +1,29 @@
-# 최소공배수
-import math
+#최소공배수 구하기3
+# 유클리드 호제법으로 구하기
+
 import sys
 
 times = int(input())
 
-for i in range(times):
+# 최대공약수
+def gcd(a,b):
+    while(b != 0):
+        a, b = b, a % b
+    return a
 
-    number = list(map(int,sys.stdin.readline().rstrip('\n').split(' ')))
+# 최소공배수
+def lcm(a,b,g):
+    return int(g * (a/g) * (b/g))
+    
+
+for i in range(times):
+    
+    number = list(map(int,sys.stdin.readline().rstrip('/n').split(' ')))
+    
     X = number[0]
     Y = number[1]
     
-    print(math.lcm(X, Y))
+    gc = gcd(X,Y)
+    lc = lcm(X, Y, gc)
     
+    print(lc)
