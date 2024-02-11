8cc727b7518f8a38a210e2e72472a00f90d9f836
Eui Hyeok Song <dmlgur0517@daum.net>
2024-02-11 14:42:39 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,9 +1,24 @@
-import math
+# for문을 이용해서 최소공배수 / 최대공약수 구하기
+# 시간 초과 발생 -> 최대공약수를 이용해서 최소공배수 구하기
 
-number = list(input().split(' '))
+# 입력받은 str을 int형의 list로 저장
+number = list(map(int,input().split(' ')))
+              
+A = number[0]
+B = number[1]
 
-A = int(number[0])
-B = int(number[1])
+# 최대공약수 구하기
+for i in range(min(A,B),0,-1):
+    
+    if(A % i == 0 and B % i == 0):
+        print(i)
+        break
+       
+print(int(i*(A/i)*(B/i)))
+        
+# 최소공배수 구하기
+# for j in range(max(A,B),(A*B)+1):
+#     if(j % A == 0 and j % B == 0):
+#         print(j)
+#         break
 
-print(math.gcd(A,B))
-print(math.lcm(A,B))
