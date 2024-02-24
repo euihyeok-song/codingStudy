15399b135d8fe4febeb074d700a5bfdc1b027017
Eui Hyeok Song <dmlgur0517@daum.net>
2024-02-24 17:45:03 +0900

    [Silver III] Title: 이친수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -4,12 +4,11 @@
 N = int(input())
 
 # 행(col)은 N자릿수 열(row)는 앞에 오는 수(0,1)
-dp = [0 for i in range(91)]
+dp = [0 for i in range(N+1)]
 
 dp[1] = 1
-dp[2] = 1
 
-for j in range(3,91):
+for j in range(2,N+1):
     
     dp[j] = dp[j-2] + dp[j-1]
     
