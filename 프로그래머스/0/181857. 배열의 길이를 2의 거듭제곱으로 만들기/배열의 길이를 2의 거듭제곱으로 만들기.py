f5683de9a9d227e7c9bf5de7ea72a87543b739a9
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-04 17:41:32 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.py
@@ -0,0 +1,14 @@
+def solution(arr):
+    answer = []
+    length = len(arr)
+    num = 1
+    
+    while(num < length):
+        
+        num *= 2
+
+    for i in range(num-length):
+        arr.append(0)
+    
+    answer = arr
+    return answer
