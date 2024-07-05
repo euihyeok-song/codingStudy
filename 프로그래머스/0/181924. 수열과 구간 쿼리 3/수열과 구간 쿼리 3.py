762740cb3d1ea1e0939820a858bd69c17bd999b8
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-05 14:00:48 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -1,11 +1,14 @@
 def solution(arr, queries):
     answer = []
-    tmp = 0
+    # tmp = 0
     
-    for i in range(len(queries)):
-        tmp = arr[queries[i][0]]
-        arr[queries[i][0]] = arr[queries[i][1]]
-        arr[queries[i][1]] = tmp
+    # for i in range(len(queries)):
+    #     tmp = arr[queries[i][0]]
+    #     arr[queries[i][0]] = arr[queries[i][1]]
+    #     arr[queries[i][1]] = tmp
     
+    for a, b in queries:
+        arr[a], arr[b] = arr[b], arr[a]
     answer = arr
+
     return answer
