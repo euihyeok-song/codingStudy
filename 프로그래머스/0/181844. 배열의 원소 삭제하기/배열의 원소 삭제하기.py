aba93a2e3366565f7e871db661eaab27bd7d53c1
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-05 10:00:02 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -0,0 +1,12 @@
+def solution(arr, delete_list):
+    answer = []
+    
+    for i in range(len(arr)):
+        flag = 0
+        for j in range(len(delete_list)):
+            if(arr[i] == delete_list[j]):
+                flag = 1
+        if flag != 1:
+            answer.append(arr[i])
+    
+    return answer
