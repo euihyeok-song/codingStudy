7c3dab46075c7605c0182ae885819aead2df99ce
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-06 16:01:43 +0900

    [level 0] Title: 배열 만들기 6, Time: 24.80 ms, Memory: 10.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    stk = []
+    i = 0
+    
+    while(i < len(arr)):
+        
+        if stk and stk[-1] == arr[i]:
+            stk.pop()
+            i += 1
+        elif stk and stk[-1] != arr[i]:
+            stk.append(arr[i])
+            i += 1
+        elif not stk:
+            stk.append(arr[i])
+            i += 1
+            
+    if not stk:
+        stk.append(-1)
+            
+    return stk
