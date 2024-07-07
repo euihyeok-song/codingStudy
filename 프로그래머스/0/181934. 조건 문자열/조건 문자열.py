43984a7b0f1c8e1e5416925b62a5bfd6b6fa4699
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-07 11:12:45 +0900

    [level 0] Title: 조건 문자열, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.py
@@ -1,6 +1,9 @@
 def solution(ineq, eq, n, m):
     answer = 0
     
+    # eval 함수는 보안상의 이유로 잘 쓰이지는 않는다.
+    # answer = int(eval(str(n) + ineq + eq.replace('!','') +str(m)))
+    
     if eq == '!' and ineq == '>':
         answer = bool(n - m > 0)
     elif eq == '!' and ineq == '<':
