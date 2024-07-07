d222c671f4fb7ddcdb5ecdc785d4589243b01421
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-07 11:28:00 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 1.79 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.py
@@ -1,8 +1,11 @@
 def solution(my_string, queries):
     string = list(my_string)
     
+    # for a,b in queries:
+    #     string[a:b+1] = reversed(string[a:b+1])
+    
     for a,b in queries:
-        string[a:b+1] = reversed(string[a:b+1])
+        string[a:b+1] = string[a:b+1][::-1]
         
     answer = ''.join(string)
     return answer
