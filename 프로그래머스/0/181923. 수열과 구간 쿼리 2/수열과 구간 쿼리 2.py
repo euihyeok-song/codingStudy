126b08f92c00f0d0c9cefb81c1cc89b825d73f60
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-08 18:28:27 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 8.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,11 +1,22 @@
 def solution(arr, queries):
-    answer = [-1] * len(queries)
+    answer = []
     
-    for idx, val in enumerate(queries):
-        min = 1000000
-        for i in range(val[0],val[1]+1):
-            if(arr[i] > val[2] and arr[i] < min):
-                min = arr[i]
-                answer[idx] = min
-                
+    # 아래는 너무 cost가 많이 드는 코드
+    # answer = [-1] * len(queries)
+    # for idx, val in enumerate(queries):
+    #     min = 1000000
+    #     for i in range(val[0],val[1]+1):
+    #         if(arr[i] > val[2] and arr[i] < min):
+    #             min = arr[i]
+    #             answer[idx] = min
+    
+    # min함수를 써서 다시 짠 코드
+    for a,b,c in queries:
+        tmp = []
+        for idx in range(a,b+1):
+            if(arr[idx] > c):
+                tmp.append(arr[idx])
+        # tmp가 비어있는 경우를 생각해서 조건문으로 넣어줘야 함
+        answer.append(-1 if not tmp else min(tmp)) 
+        
     return answer
