d43ce36227cf856be11b44683d894d7414bc59c1
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-10 19:01:32 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.py
@@ -11,4 +11,8 @@ def solution(num_list):
                 val = (val-1)/2
             answer += 1
     
+    # 모범 코드 => 2진수에서는 Ob11010과 같이 나오는데 1을 남기려면 ob와 마지막 수를 제외하고
+    # 모두 삭제시켜야 하는데, 이는 2로 나눈 횟수와 동일
+    # answer = sum(len(bin(i)) - 3 for i in num_list)
+    
     return answer
