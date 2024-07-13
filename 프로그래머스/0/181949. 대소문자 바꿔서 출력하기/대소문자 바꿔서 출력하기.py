a97665e32e175882cd2f7e47fd62d82e1208d93a
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-13 15:47:35 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 11.76 ms, Memory: 7.23 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,13 +1,16 @@
 str = input()
 
-answer = list(str)
-result = []
+# answer = list(str)
+# result = []
 
-for val in answer:
-    if( val.isupper() ):
-        result.append(val.lower())
-    else:
-        result.append(val.upper())
+# for val in answer:
+#     if( val.isupper() ):
+#         result.append(val.lower())
+#     else:
+#         result.append(val.upper())
         
-str = ''.join(result)
-print(str)
+# str = ''.join(result)
+# print(str)
+
+# 모범답안
+print(str.swapcase())
