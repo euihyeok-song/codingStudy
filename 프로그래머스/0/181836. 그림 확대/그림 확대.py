37cc60163d84b36b3f189ad171d5fd82fbd7c62e
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-13 15:41:01 +0900

    [level 0] Title: 그림 확대, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.py
@@ -2,15 +2,21 @@ def solution(picture, k):
     result = []
     answer = []
     
-    for val in picture:
-        word = ''
-        for idx in val:
-            word += idx * k
-        result.append(word)
+#     for val in picture:
+#         word = ''
+#         for idx in val:
+#             word += idx * k
+#         result.append(word)
         
-    for i in result:
-        for j in range(k):
-            answer.append(i)
+#     for i in result:
+#         for j in range(k):
+#             answer.append(i)
+
+
+    # 모범 답안 (replace 사용)
+    for i in picture:
+        for _ in range(k):
+            answer.append(i.replace('.','.'*k).replace('x','x'*k))
     
         
     return answer
