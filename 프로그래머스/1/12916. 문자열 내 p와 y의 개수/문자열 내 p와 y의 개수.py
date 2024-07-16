1ebed5e31cddf05be97002765e868b602e89523e
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-16 18:18:20 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -1,15 +1,19 @@
 def solution(s):
     answer = True
-    pcount = 0
-    ycount = 0
+#     pcount = 0
+#     ycount = 0
     
-    for val in list(s):
-        if(val == 'p' or val == 'P'):
-            pcount += 1
-        elif(val == 'y' or val == 'Y'):
-            ycount += 1
+#     for val in list(s):
+#         if(val == 'p' or val == 'P'):
+#             pcount += 1
+#         elif(val == 'y' or val == 'Y'):
+#             ycount += 1
     
-    if(pcount != ycount):
+#     if(pcount != ycount):
+#         answer = False
+
+#   모범답안
+    if(s.lower().count('y') != s.lower().count('p')):
         answer = False
     
     return answer
