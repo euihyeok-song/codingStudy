d01578fa6f4e45cba5b5d3580026b27cdfec307f
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-16 18:24:04 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 3.65 ms, Memory: 13.4 MB -BaekjoonHub

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -5,4 +5,5 @@ def solution(arr, divisor):
         if val % divisor == 0:
             answer.append(val)
         
-    return sorted(answer) if answer else [-1]
+    # 앞의 부분이 false일 경우에 뒤에 부분이 출력됨    
+    return sorted(answer) or [-1]
