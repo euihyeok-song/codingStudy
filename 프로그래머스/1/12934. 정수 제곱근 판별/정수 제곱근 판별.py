9e32ce457a687227271d421ae9fed5d3b194d941
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-16 13:53:29 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -1,13 +1,22 @@
-import math
+# import math
 
 def solution(n):
     answer = 0
-    num = int(math.sqrt(n))
     
-    if(num * num == n):
-        answer = (num+1) * (num+1)
+#     num = int(math.sqrt(n))
+    
+#     if(num * num == n):
+#         answer = (num+1) * (num+1)
+#     else:
+#         answer = -1
+    
+    # 모범답안
+    
+    sqrt = n ** (1/2)
+    
+    if(sqrt % 1 == 0):
+        answer = (sqrt+1) ** 2
     else:
         answer = -1
-    
         
     return answer
