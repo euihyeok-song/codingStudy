7d7e2bbeaa7f9a4678190dbbaa5e56a02e51c14e
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-16 18:47:12 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,11 +1,17 @@
 def solution(seoul):
     answer = ''
-    index = 0
+#     index = 0
     
-    for idx,val in enumerate(seoul):
-        if val == 'Kim':
-            index = idx
+#     print(index('Kim'))
     
-    answer = "김서방은 " + str(index) + "에 있다"
+#     for idx,val in enumerate(seoul):
+#         if val == 'Kim':
+#             index = idx
+    
+#     answer = "김서방은 " + str(index) + "에 있다"
+    
+    # 모법답안
+    
+    answer = "김서방은 " + str(seoul.index('Kim')) + "에 있다"
     
     return answer
