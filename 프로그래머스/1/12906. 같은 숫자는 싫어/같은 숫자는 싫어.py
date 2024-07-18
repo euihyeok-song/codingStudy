f08bb1ea5789410ff3431073c5e06bc6a9ce6efa
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-18 17:36:29 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 42.47 ms, Memory: 27.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,12 @@
+def solution(arr):
+    
+    answer = []
+    last = -1
+    
+    for i in arr:
+        if(i != last):
+            answer.append(i)
+        last = i
+    
+    
+    return answer
