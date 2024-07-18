4c36b6a4d404eb7539423aa35b00e0d2d51db246
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-18 22:03:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -2,15 +2,24 @@ def solution(n):
     answer = 0
     three = []
     
-    # 3진법으로 만들기
-    while(n != 0): 
+#     # 3진법으로 만들기
+#     while(n != 0): 
         
-        three.append(n%3)
-        n //= 3
+#         three.append(n%3)
+#         n //= 3
         
-    three.reverse()
+#     three.reverse()
     
-    for i in range(len(three)):
-        answer += three[i] * (3**i)
+#     for i in range(len(three)):
+#         answer += three[i] * (3**i)
+
+    tmp = ''
+    while n:
+        tmp += str(n % 3)
+        n = n // 3
+
+    # 3진법으로 만들어줌
+    answer = int(tmp, 3)
+    
     
     return answer
