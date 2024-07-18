c975c78f3a133e68a32d89646961c4599ef22430
Eui Hyeok Song <dmlgur0517@daum.net>
2024-07-18 22:45:06 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,10 +1,17 @@
+from itertools import combinations
+
 def solution(number):
     answer = 0
     
-    for i in range(len(number)-2):
-        for j in range(i+1,len(number)-1):
-            for k in range(j+1,len(number)):
-                if(number[i]+number[j]+number[k] == 0):
-                    answer += 1
+    # for i in range(len(number)-2):
+    #     for j in range(i+1,len(number)-1):
+    #         for k in range(j+1,len(number)):
+    #             if(number[i]+number[j]+number[k] == 0):
+    #                 answer += 1
         
+    # 모범답안  combinations(배열, 한 그룹에 들어갈 숫자 갯수)
+    for i in combinations(number,3):
+        if sum(i) == 0:
+            answer += 1
+    
     return answer
