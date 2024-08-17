293fc740a477245bf2d6162e2a59db03038bcfe3
EuiHyeok <dmlgur0517@daum.net>
2024-08-17 12:12:44 +0900

    [PYTHON] DFS

--- working/2602.py
+++ working/2602.py
@@ -1,28 +1,24 @@
 # 2602법 - 바이러스 (DFS)
 # 1번 컴퓨터와 연결되어 있는 모든 컴퓨터를 찾아야 하기 때문에 DFS 사용
 
-def dfs(graph,visit,start):
+def dfs(start):
     
-    count = 0
-    visit[0][0] = True
+    visit[start] = 1
     
-    for i in range(len(graph)):
-        if graph[start][i] == 1 and visit[start][i] == False:
-            visit[start][i] = True
-            count += 1
-            dfs(graph,visit,i)
-    
-    return count
+    for i in range(1,len(graph)):
+        if graph[start][i] == 1 and visit[i] == 0:
+            dfs(i)
 
 N = int(input())
 M = int(input())
 
-graph = [[0]*(M+1) for _ in range(M)]
-visit = [[False]*M for _ in range(M)]
-start = 0
+graph = [[0]*(N+1) for _ in range(N+1)]
+visit = [0 for _ in range(N+1)]
+start = 1
 
-for _ in range(M):
-    a , b = map(int,input().split(" "))
+for _ in range(1,M+1):
+    a, b = map(int,input().split(" "))
     graph[a][b] = graph[b][a] = 1
     
-print(dfs(graph,visit,start))
+dfs(start)    
+print(sum(visit)-1)
