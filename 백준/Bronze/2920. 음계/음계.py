269ab66087f77f8c92a0e0abd5d3de7eb677ac42
Eui Hyeok Song <dmlgur0517@daum.net>
2024-12-22 11:38:23 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,11 @@
+num = list(map(int,input().split()))
+asc = sorted(num)
+desc = sorted(num, reverse=True)
+
+if(num== asc):
+    print("ascending")
+elif(num == desc):
+    print("descending")
+else:
+    print("mixed")
+
