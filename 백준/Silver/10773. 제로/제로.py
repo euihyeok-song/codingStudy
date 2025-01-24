de98961e7be4f737155e2378e8ef56c0240c03d3
Eui Hyeok Song <dmlgur0517@daum.net>
2025-01-25 00:22:52 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
+K = int(input())
+
+stack = []
+
+for _ in range(K):
+    
+    num = int(input())
+    
+    if num == 0 and stack:
+        stack.pop()
+    else:
+        stack.append(num)
+    
+print(sum(stack))
