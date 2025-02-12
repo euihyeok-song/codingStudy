ac033988d75beebebdcc2a8000e42cbce85fe142
Eui Hyeok Song <dmlgur0517@daum.net>
2025-02-12 14:44:26 +0900

    [Silver IV] Title: 좋은 단어, Time: 112 ms, Memory: 33976 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+cnt = 0
+
+for _ in range(N):
+    
+    word = list(input().rstrip())
+    stack = []
+    
+    # A든 B든 홀수개가 들어오면 Pass
+    if word.count('A') % 2 != 0 or word.count('B') % 2 != 0:
+        continue
+    
+    for val in word:
+        
+        if stack and stack[-1] == val:
+            stack.pop()
+        else:
+            stack.append(val)
+            
+    if not stack:
+        cnt += 1
+    
+print(cnt)
