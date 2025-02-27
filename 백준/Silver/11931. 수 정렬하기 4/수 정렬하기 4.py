3903a1fc1803016a0b678cdc6f463d149901f79c
Eui Hyeok Song <dmlgur0517@daum.net>
2025-02-27 22:52:19 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1336 ms, Memory: 85612 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+arr = [0] * N
+
+for i in range(N):
+    arr[i] = int(input().rstrip())
+    
+for j in sorted(arr,reverse=True):
+    print(j)
