102b1882d4997532834d1f043cc588bde74d7050
Eui Hyeok Song <dmlgur0517@daum.net>
2025-04-18 23:46:04 +0900

    [level 2] Title: 괄호 회전하기, Time: 330.93 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(s):
+    
+    dq = deque(list(s))
+    cnt = 0
+    
+    for _ in range(len(s)):
+        dq.append(dq.popleft())
+        st = "".join(dq)
+        
+        prev = None
+        while prev != st:
+            prev = st
+            st = st.replace("()", "").replace("{}", "").replace("[]", "")
+            
+        if st == "":
+            cnt += 1
+        
+    return cnt
