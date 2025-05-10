092e0e06f51f379101b6a713804078e21a3bcca2
Eui Hyeok Song <dmlgur0517@daum.net>
2025-05-10 21:04:19 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,13 @@
+# 점수, 사번, 성명, 직책, 이메일
+SELECT 
+       SUM(SCORE) AS SCORE,
+       A.EMP_NO,
+       A.EMP_NAME,
+       A.POSITION,
+       A.EMAIL
+  FROM HR_EMPLOYEES A
+  JOIN HR_GRADE B ON A.EMP_NO = B.EMP_NO
+ WHERE YEAR = 2022
+ GROUP BY EMP_NO
+ ORDER BY SCORE DESC
+ LIMIT 1;
